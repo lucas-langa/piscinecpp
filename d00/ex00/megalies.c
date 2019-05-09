c63eaaa87dd0613a900a90d417b97b2530aba038
Lucas <llanga@student.wethinkcode.co.za>
2019-05-09 23:43:05 +0200

    Update megalies.c

--- d00/ex00/megalies.c
+++ d00/ex00/megalies.c
@@ -1,54 +1,75 @@
-// #include <iostream>
-#include <ctype.h>
-#include <stdio.h>
-#include <unistd.h>
-
-void 	ft_putstr(char *str)
-{
-	while (*str)
-	{
-		write(1, *&str, 1);
-		str++;
-	}
-	return ;
-}
+#include <iostream>
 
 void	disp(char **av, int ac)
+
 {
+
 	int i;
+
 	int j;
 
+
+
 	i = 1;
+
 	while (i < ac)
+
 	{
+
 		j = 0;
+
 		while (av[i][j] != '\0')
+
 		{
+
 			//std::cout << 
-//			putchar(toupper(av[i][j]));
+
+
 			av[i][j] = toupper(av[i][j]);
+
 			j++;
+
 		}
-		ft_putstr(av[i]);
-		// std::cout << av[i];
-		putchar(' ');
+		std::cout << av[i];
+
+		std::cout << " ";
+
 		i++;
+
 	}
-	ft_putstr(" ");
-	// std::cout << std::endl;
+
+	//std::cout " " << std::endl;
+
+	std::cout << std::endl;
+
 	return ;
+
 }
 
+
+
 int	main( int ac, char **av )
+
 {
+
 	if (ac == 1)
+
 	{
-		/*std::cout <<*/ ft_putstr("* LOUD AND UNBEARABLE FEEDBACK NOISE *");/* << std::endl;*/
+
+		std::cout <<"* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+
 		(void)av;
+
 	}
+
 	else
+
 	{
+
 		disp(av, ac);
+
 	}
+
 	return (0);
+
 }
