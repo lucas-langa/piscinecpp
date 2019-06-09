ab852c7453b13789bc34fd9ffae5acee46a9a82c
Lucas LANGA <llanga@c5r1s8.wethinkcode.co.za>
2019-06-09 15:07:55 +0200

    i need a primer on this

---
+++ notions/sub-typing polymorphism/poly1.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+
+class Character {
+	public:
+		virtual void 	sayHello( std::string const & target );
+};
+
+class Warrior : public Character {
+	public:
+		virtual void 	sayHello( std::string const & target );
+};
+
+class Cat {
+	// [...]
+};
+
+void 	Character::sayHello( std::string const & target ) {
+	std::cout << "Hello " << target << " !" << std::endl;
+	return ;
+};
+
+void 	Warrior::sayHello( std::string const & target) {
+	std::cout << "F*** off " << target << " , I don't like you !" << std::endl;
+	return ;
+};
+
+int		main(void) {
+	// This is fine, Warrior is a Warrior
+	Warrior*	a =   new Warrior();
+
+	//This is also ok because warrior is a character
+	Character*	b = new Warrior();
+
+	// This would not be fine because Character is not a warrior
+	//Although they are related, and W is a C, the reverse is not tru
+	// Warrior*  c = new Character();
+
+	// This wouldn't work either because Cat is not a character
+	//THey aren't even related
+	//Character* d = new Cat();
+
+	a->sayHello("students");
+	b->sayHello("students");
+	return (0);
+}
