52b255fcdf98d7f3f134d0615d78ebc8faedb386
Lucas LANGA <llanga@c5r1s8.wethinkcode.co.za>
2019-06-13 12:14:20 +0200

    ,.

--- notions/casts/typequalifier.c
+++ notions/casts/typequalifier.c
@@ -5,7 +5,7 @@ int 	main(void ) {
 	int	 		const * c = (int const *) &a; //explicit type qualifier cast
 	
 	int 		const * d = &a; 	//implicit promotion ->iok
-	int 		*e = d;				//implicit demotion ->fuck no!
+	int *		e = d;				//implicit demotion ->fuck no!
 	int *		f = (int *)d;		//exlicit demotion -> ok, i obey
 
 	return (0);
